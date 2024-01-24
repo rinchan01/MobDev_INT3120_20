2e225b74bc490147aacc5ecd9b3f27d46ba8297f
Trịnh Thái Linh <100512203+rinchan01@users.noreply.github.com>
2024-01-24 20:14:27 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.helloworld.ui.theme.HelloWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting()
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+        Text(
+            text = "Hello World!"
+        )
+}
+
