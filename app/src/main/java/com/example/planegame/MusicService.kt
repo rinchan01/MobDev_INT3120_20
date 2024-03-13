a793b7148aab9d7c9b9626a0bf37617a5e657323
DucNM <21020620@vnu.edu.vn>
2024-03-13 23:04:56 +0700

    simple settings with music

---
+++ app/src/main/java/com/example/planegame/MusicService.kt
@@ -0,0 +1,24 @@
+package com.example.planegame
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import android.provider.Settings
+
+class MusicService :Service(){
+    lateinit var mediaPlayer: MediaPlayer
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        mediaPlayer = MediaPlayer.create(this, R.raw.sample_music)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+        return START_STICKY
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.stop()
+    }
+}
