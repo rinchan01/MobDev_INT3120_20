8c5c2bac688d35835a11eb6162db4c6f0ad42723
rinchan01 <thailinh8426@gmail.com>
2024-03-21 14:15:31 +0700

    add result game score

--- app/src/main/java/com/example/planegame/GameResultActivity.kt
+++ app/src/main/java/com/example/planegame/GameResultActivity.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import com.example.planegame.R.*
 
 class GameResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_result)
+        setContentView(layout.activity_game_result)
 
-        findViewById<Button>(R.id.buttonBackToMenu).setOnClickListener {
+        val score = intent.getIntExtra("score", 0)
+        findViewById<TextView>(R.id.score).text = "Your score: $score"
+        findViewById<Button>(id.buttonBackToMenu).setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
