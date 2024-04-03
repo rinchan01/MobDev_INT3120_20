99fbe75c3e555048058a7366fcbfe7a1e2d6902d
DucNM <21020620@vnu.edu.vn>
2024-04-03 11:05:23 +0700

    webview

--- app/src/main/java/com/example/planegame/HelpActivity.kt
+++ app/src/main/java/com/example/planegame/HelpActivity.kt
@@ -1,20 +1,36 @@
 package com.example.planegame
 
-import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.webkit.WebView
-import android.widget.ImageView
+import android.webkit.WebViewClient
+import androidx.activity.addCallback
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class HelpActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_help)
-        val myWebView: WebView = findViewById(R.id.webview)
-        // enable javascript
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.loadUrl("https://rinchan01.github.io/help-web/")
+        val webView = findViewById<WebView>(R.id.webview)
+
+        webView.webViewClient = WebViewClient()
+
+        webView.apply {
+            loadUrl("https://rinchan01.github.io/help-web/")
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+        }
+
+        onBackPressedDispatcher.addCallback(this) {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                finish()
+            }
+        }
     }
 
 }
